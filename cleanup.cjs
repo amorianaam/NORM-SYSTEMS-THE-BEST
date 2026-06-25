09f758c56501e141178645623a80c09f300f5463
3MORI <168870818+amorianaam@users.noreply.github.com>
2026-06-25 05:22:30 +0300

    Delete cleanup.cjs

--- cleanup.cjs
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const Database = require('better-sqlite3');
-const db = new Database('database.db');
-const stmt = db.prepare("DELETE FROM content_blocks WHERE block_key LIKE '%_track' AND page = 'contactPage'");
-const info = stmt.run();
-console.log(`Deleted ${info.changes} rows.`);
